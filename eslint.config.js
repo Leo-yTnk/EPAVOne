c8a3e144cb3d54b01cacbcd2efc2141e3754f8ef
Leo-yTnk <leonardo.yukio.tanaka@gmail.com>
2026-09-26 11:53:04 -0300

    Add architecture foundation: eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import js from '@eslint/js';
+import globals from 'globals';
+
+export default [
+  {
+    ignores: ['dist/**', 'coverage/**', 'node_modules/**']
+  },
+  js.configs.recommended,
+  {
+    files: ['**/*.{js,jsx,mjs}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      },
+      parserOptions: {
+        ecmaFeatures: { jsx: true }
+      }
+    },
+    rules: {
+      'no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+      'no-console': ['warn', { allow: ['warn', 'error'] }]
+    }
+  }
+];
